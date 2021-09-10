977e66a2e8b9ff90841a3d22d9c02fabb60f6d70
Joseph <joseph@lightningkite.com>
2021-09-10 16:13:58 -0600

    Initial commit

---
+++ Example/XmlToXibRuntime/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  XmlToXibRuntime
+//
+//  Created by UnknownJoe796 on 09/10/2021.
+//  Copyright (c) 2021 UnknownJoe796. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
